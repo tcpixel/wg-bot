70d0501d87502a3488bf7d3d9f471ad9f60d4d33
Nino <31443086+tcpixel@users.noreply.github.com>
2024-05-25 22:18:42 +0200

    adding function for Summer Breeze Ticket notification

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import discord
 from config import *
 from discord.ext import commands, tasks
 import asyncio
+import requests
+from bs4 import BeautifulSoup
 
 # Initialize bot
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
+allowed_mentions = discord.AllowedMentions.all
+lastStatusCode=0
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
     daily_cleanup.start()
+    ticket_request.start()
 
 
 #####
@@ -48,6 +53,49 @@ async def daily_cleanup():
     async for message in channel.history():
         if "~~" in message.content:
             await message.delete()
+
+
+#####
+# Summer Breeze
+#####
+@tasks.loop(minutes=5)
+async def ticket_request():
+    print("Request wird durchgeführt.")
+    channel = bot.get_channel(int(summerBreezeChannelId))
+    result = requests.get('https://www.sbtix.de/swp/for/69602-tickets-fruehanreise-slots-dienstag-sinbronn-dinkelsbuehl-am-13-08-2024', verify=False)
+    lastStatusCode = result.status_code
+
+    if (lastStatusCode!=200) and (result.status_code!=200):
+        content = "@"+ninoUserId+" Es ist ein Fehler aufgetreten. Die Website ist nicht erreichbar.\nStatus Code: "+result.status_code
+        await channel.send(content, allowed_mentions=allowed_mentions)
+        return 0
+
+    soup = BeautifulSoup(result.content, 'html.parser')
+    tabellenZeilen = soup.find_all('tr')
+    for zeile in tabellenZeilen:
+        spalten = zeile.find_all('td')
+        if spalten:
+            ticket = str(spalten[0].contents[0])
+            if ("Anhängerticket" in ticket) or ("Green Camping" in ticket) or ("Comfort Camping" in ticket):
+                continue
+            else:
+                content = "@everyone Ein neues Ticket ist verfügbar.\n"+ticket+"\nhttps://www.sbtix.de/swp/for/69602-tickets-fruehanreise-slots-dienstag-sinbronn-dinkelsbuehl-am-13-08-2024"
+                await channel.send(content, allowed_mentions=allowed_mentions)
+
+    # Zweite Abfrage
+    result = requests.get('https://www.sbtix.de/swp', verify=False)
+
+    if (lastStatusCode!=200) and (result.status_code!=200):
+        content = "@"+ninoUserId+" Es ist ein Fehler aufgetreten. Die Website ist nicht erreichbar.\nStatus Code: "+result.status_code
+        await channel.send(content, allowed_mentions=allowed_mentions)
+        return 0
+
+    soup = BeautifulSoup(result.content, 'html.parser')
+    tabellenZeilen = soup.find_all('div', 'col-md-3 col-sm-6')
+    for zeile in tabellenZeilen:
+        if "Frühanreise Slots - Montag" in zeile.contents[0]:
+            content = "@everyone Es scheint ein neues Ticket verfügbar zu sein für die Frühanreise am Montag:\n"+str(zeile.contents[0])+"\nhttps://www.sbtix.de/swp\nBitte überprüfen."
+            await channel.send(content, allowed_mentions=allowed_mentions)
 
 # Run the bot
 bot.run(botToken)
